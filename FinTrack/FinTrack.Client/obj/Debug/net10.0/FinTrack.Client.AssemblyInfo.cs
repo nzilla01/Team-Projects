e9a38d5ca32e24eb549f6fc5466c41d28db5a4e1
nzilla <victorbliss044@gmail.com>
2026-06-10 03:12:06 +0100

    commit

--- FinTrack/FinTrack.Client/obj/Debug/net10.0/FinTrack.Client.AssemblyInfo.cs
+++ FinTrack/FinTrack.Client/obj/Debug/net10.0/FinTrack.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinTrack.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b119a9bf7e7d903a48a2b9a87ead939926af8f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinTrack.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinTrack.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- FinTrack/FinTrack.Server/obj/Debug/net10.0/FinTrack.Server.AssemblyInfo.cs
+++ FinTrack/FinTrack.Server/obj/Debug/net10.0/FinTrack.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinTrack.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a2e59ef72246d638df6782ab708e900f8da6e24")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinTrack.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinTrack.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
